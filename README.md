1d3fb39d1d442f46a05ed77b939d958a511c30db
Spencer Fraint <spencer@intentionet.com>
2018-11-09 18:34:26 -0800

    add files for initial commit

--- README.md
+++ README.md
@@ -1,2 +1,109 @@
 # ansible-demo
-Repository for Ansible Demo
+This repo contains the files and instructions necessary to reproduce our [Ansible demo presentation](https://www.ansible.com/resources/webinars-training/validating-pre-commit-network-configuration-changes-at-scale-with-batfish-ansible), which uses [Pybatfish](https://github.com/batfish/pybatfish) and [Batfish](https://github.com/batfish/batfish) to validate pre-commit network configuration changes.  
+
+Automation without pre-commit validation is risky. Use Batfish in your automated workflow for comprehensive correctness guarantees before pushing to production.
+
+
+## Prereqs
+
+* Run [Batfish](https://github.com/batfish/batfish).  If you don't already have it set up and running, run this to set up and start the [docker container](https://github.com/batfish/docker/blob/master/batfish.md):
+
+  `mkdir -p data && docker run -d -v $(pwd)/data:/data -p 9997:9997 -p 9996:9996 batfish/allinone:sha_736c753_e840243`
+* Install the [Pybatfish](https://github.com/batfish/pybatfish) version used for the demo (to guarantee compatibility):
+
+  `pip install git+git://github.com/batfish/pybatfish.git@386d0379e0b16985cee9c6cd12c0e7d8d8c8d6cc`
+* Install Ansible (see [installation guide](https://docs.ansible.com/ansible/2.7/installation_guide/intro_installation.html) for details):
+
+  `pip install ansible`
+
+## Setup
+* Clone this demo repo:
+
+  `git clone git@github.com:batfish/ansible-demo.git`
+* Edit the group vars in `playbooks/inventory` file to reflect your setup.
+* Run the setup script to create the base snapshot and policies:
+
+  `python python/demo-setup.py -p snapshots/snapshot0/ -a "DC Fabric Policy" checks/fabric/ -a "DC Base Policy" checks/base/`
+
+### Optional Setup for Integrations
+
+#### Git Integration
+This Git integration allows Ansible to create a branch in the GitHub repository with your network configurations, and commit each change you validate to that branch.
+* Clone your network repo e.g.
+  `git clone git@github.com:YOUR_ORG_NAME/YOUR_NETWORK_REPO.git`
+
+  Note: The network repo should be formatted the same way as `snapshots/snapshot0/`, with config files in a `configs/` dir at the root of the repo.
+* Generate a [GitHub token with access to this repo](https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/), then supply the token to the `playbooks/inventory` file
+
+#### S3 Integration
+This S3 integration is for posting Batfish validation logs to your S3 bucket. Logs are also stored locally whether or not S3 is used.
+* [Create an S3 bucket](https://docs.aws.amazon.com/AmazonS3/latest/gsg/CreatingABucket.html) to store your logs
+* Install required Python modules:
+
+  `pip install botocore`
+  `pip install boto3`
+
+#### Slack Integration
+* Follow [these instructions](https://get.slack.help/hc/en-us/articles/115005265063-Incoming-WebHooks-for-Slack) to create a Slack service that accepts incoming webhooks.
+
+## Running the Demo
+
+Both scenarios can optionally be run with extra tags `s3`, `slack`, and/or `git` to enable different integrations.
+
+Note: logs for each playbook run are written to `s3_logs/`.
+
+### Add Leaf Scenario
+This scenario adds a new leaf router to an existing datacenter and confirms the changes made adhere to the defined network policies.
+
+#### Run 1 - Fail Policy
+This run fails due to duplicate BGP ASNs between leaf 2 and leaf 3.
+
+* Run the playbook `ansible-playbook -i playbooks/inventory playbooks/master.yml --tags "always"`
+* Fill in the prompts:
+  * Hostname: `lhr-leaf-03`
+  * POD: `1`
+  * BGP ASN: `65002`
+
+#### Run 2 - Pass Policy
+This run passes our predefined policies.
+
+* Run the playbook `ansible-playbook -i playbooks/inventory playbooks/master.yml --tags "always"`
+* Fill in the prompts:
+  * Hostname: `lhr-leaf-03`
+  * POD: `1`
+  * BGP ASN: `65003`
+
+
+### Update ACL Scenario
+This scenario is derived from our `Provably Safe ACL and Firewall Changes` [Python notebook](https://github.com/batfish/pybatfish/blob/master/jupyter_notebooks/Provably%20Safe%20ACL%20and%20Firewall%20Changes.ipynb).  It updates our firewalls to allow access to a new HTTP service (TCP port 80 on subnet 10.1.5.0/27) and confirms the changes:
+1) Are necessary (i.e. firewalls do not currently permit the new traffic)
+2) Allow the new traffic
+3) Don't allow anything other than the new traffic (i.e. confirm no collateral damage)
+
+#### Run 1 - Fail ACL Validation
+This run creates a bigger hole in the firewall than we intended, thus fails collateral damage check.
+
+* Edit the file `inputs/acls.json` to reflect the desired ACL changes, adding this line just before the deny all line: `"permit tcp any 10.1.5.0 0.0.0.63 eq 80",`
+* Run the ACL playbook: `ansible-playbook -i playbooks/inventory playbooks/master_acl.yml --tags "create"`
+* Fill in the prompts:
+  * Firewall hostnames: `lhr-fw-01|lhr-fw-02`
+  * POD: `1`
+  * Source IPs of new traffic to allow: `0.0.0.0/0`
+  * Destination IPs of new traffic to allow: `10.1.5.0/27`
+  * IP protocol for new traffic to allow: `tcp`
+  * Destination ports: `80`
+
+#### Run 2 - Pass ACL Validation
+This run passes our ACL validation checks.
+
+* Edit the file `inputs/acls.json` to reflect the desired ACL changes, replacing the line added above with this line: `"permit tcp any 10.1.5.0 0.0.0.31 eq 80",`
+* Run the ACL playbook: `ansible-playbook -i playbooks/inventory playbooks/master_acl.yml --tags "create"`
+* Fill in the prompts, same as the previous run:
+  * Firewall hostnames: `lhr-fw-01|lhr-fw-02`
+  * POD: `1`
+  * Source IPs of new traffic to allow: `0.0.0.0/0`
+  * Destination IPs of new traffic to allow: `10.1.5.0/27`
+  * IP protocol for new traffic to allow: `tcp`
+  * Destination ports: `80`
+
+**Got questions, feedback, or feature requests? Join our community on [Slack!](https://join.slack.com/t/batfish-org/shared_invite/enQtMzA0Nzg2OTAzNzQ1LTUxOTJlY2YyNTVlNGQ3MTJkOTIwZTU2YjY3YzRjZWFiYzE4ODE5ODZiNjA4NGI5NTJhZmU2ZTllOTMwZDhjMzA)**
---
+++ playbooks/library/batfish_policy.py
@@ -0,0 +1,210 @@
+#!/usr/bin/python
+#   Copyright 2018 Intentionet
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+ANSIBLE_METADATA = {
+    'metadata_version': '1.1',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
+
+DOCUMENTATION = '''
+---
+module: batfish_policy
+
+short_description: Runs policy in Batfish
+
+version_added: "2.7"
+
+description:
+    - "Runs (and optionally initializes) policy in Batfish, using Pybatfish"
+
+options:
+    policy_name:
+        description:
+            - Name of the policy to run.  If not specified, all analyses will be run.
+        required: false
+    host:
+        description:
+            - Host running the C(Batfish) service.
+        required: false
+    name:
+        description:
+            - Name of the snapshot to run the policy on.
+        required: true
+    network:
+        description:
+            - Name of the network containing the specified snapshot.
+        required: true
+    new:
+        description:
+            - If C(yes), a new policy is created with the supplied path.  Otherwise an existing policy is run.
+        required: false
+    path:
+        description:
+            - Path to the checks to add to the new policy.
+        required: if new is C(yes)
+
+author:
+    - Spencer Fraint (`@sfraint <https://github.com/sfraint>`_)
+
+requirements:
+    - "pybatfish"
+'''
+
+EXAMPLES = '''
+# Create and run a new policy
+- name: Create and run new policy
+  batfish_policy:
+    name: base_snapshot
+    network: test_network
+    new: yes
+    policy_name: policy_name
+    path: /path/to/policy_dir/
+
+# Run an existing policy
+- name: Run existing policy
+    name: base_snapshot
+    network: test_network
+    policy_name: policy_name
+'''
+
+RETURN = '''
+result:
+    description: Pass/Fail result of each check in the policy
+    type: str
+result_verbose:
+    description: Detailed result of each check in the policy
+    type: str
+summary:
+    description: Pass/Fail result of the policy overall
+    type: str
+'''
+
+PASS = 'PASS'
+FAIL = 'FAIL'
+
+from ansible.module_utils.basic import AnsibleModule
+
+try:
+    import json
+    from pybatfish.client.commands import (bf_get_analysis_answers, bf_init_analysis,
+                                           bf_list_analyses, bf_run_analysis,
+                                           bf_set_network, bf_session)
+    from pybatfish.datamodel.flow import HeaderConstraints
+    # noinspection PyUnresolvedReferences
+    from pybatfish.question import bfq, list_questions, load_questions  # noqa F401
+except Exception as e:
+    pybatfish_found = False
+else:
+    pybatfish_found = True
+
+
+def run_module():
+    # define the available arguments/parameters that a user can pass to
+    # the module
+    module_args = dict(
+        policy_name=dict(type='str', required=False, default=None),
+        host=dict(type='str', required=False, default='localhost'),
+        name=dict(type='str', required=True),
+        network=dict(type='str', required=True),
+        new=dict(type='bool', required=False, default=False),
+        path=dict(type='str', required=False)
+    )
+
+    # seed the result dict in the object
+    # we primarily care about changed and state
+    # change is if this module effectively modified the target
+    # state will include any data that you want your module to pass back
+    # for consumption, for example, in a subsequent task
+    result = dict(
+        changed=False,
+        result='',
+        result_verbose='',
+        summary=''
+    )
+
+    # the AnsibleModule object will be our abstraction working with Ansible
+    # this includes instantiation, a couple of common attr would be the
+    # args/params passed to the execution, as well as if the module
+    # supports check mode
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True,
+        required_if=[
+            [ "new", True, [ "path", "policy_name" ] ] # path and name are required if adding a new policy
+        ]
+    )
+
+    if not pybatfish_found:
+        module.fail_json(msg='Python module Pybatfish is required')
+
+    if module.check_mode:
+        return result
+
+    snapshot_name = module.params['name']
+    policy_name = module.params['policy_name']
+
+    try:
+        bf_session.coordinatorHost = module.params['host']
+        network = bf_set_network(module.params['network'])
+    except Exception as e:
+        module.fail_json(msg='Failed to set network: {}'.format(e), **result)
+
+    try:
+        if module.params['new']:
+            bf_init_analysis(policy_name, module.params['path'])
+    except Exception as e:
+        module.fail_json(msg='Failed to initialize policy: {}'.format(e), **result)
+
+    try:
+        if policy_name is not None:
+            policy_results = {policy_name: _run_policy(policy_name, snapshot_name)}
+        else:
+            policy_results = {a: _run_policy(a, snapshot_name) for a in bf_list_analyses()}
+    except Exception as e:
+        module.fail_json(msg='Failed to answer policy: {}'.format(e), **result)
+
+    result['result'] = {}
+    result['result_verbose'] = {}
+    failure = False
+    # If a check's summary.numFailed is 0, we assume the check PASSed
+    for policy in policy_results:
+        policy_result = policy_results[policy]
+        result['result'][policy] = {
+            k: PASS if policy_result[k]['summary']['numFailed'] == 0 else FAIL for k in policy_result
+        }
+        failure |= FAIL in result['result'][policy].values()
+
+        result['result_verbose'][policy] = {
+            k: policy_result[k]['answerElements'][0]['rows'] if 'rows' in policy_result[k]['answerElements'][0] else [] for k in policy_result
+        }
+
+    result['summary'] = FAIL if failure else PASS
+
+    module.exit_json(**result)
+
+
+def _run_policy(name, snapshot):
+    """
+    Run a policy and return a dictionary containing its checks and their results.
+    Converts check results from strings into dictionaries.
+    """
+    return {k: json.loads(v) for k, v in bf_run_analysis(name=name, snapshot=snapshot).items()}
+
+def main():
+    run_module()
+
+if __name__ == '__main__':
+    main()
